317058ae5ac46654ad004459a309f3132c386f62
zhoujs <715842648@qq.com>
2021-11-20 16:15:06 +0800

    feat:使用案例

--- components/Example.tsx
+++ components/Example.tsx
@@ -15,11 +15,10 @@ type State = {
 };
 
 export default class Example extends Component<{}, State> {
+  //模拟列表数据
   constructor(props: {}) {
     super(props);
-
     const mockList = []
-
     for (let i = 0; i < 30; i++) {
       mockList.push(
         {
@@ -29,12 +28,12 @@ export default class Example extends Component<{}, State> {
         }
       )
     }
-
     this.state = {
       list: mockList
     };
   }
 
+  //删除列表数据
   deleteDataFromList = (rowId: number) => {
     this.setState({
       list: this.state.list.filter(item => item.id !== rowId)
@@ -45,8 +44,8 @@ export default class Example extends Component<{}, State> {
     console.log('you pressed row');
   };
 
-  onHiddenAreaPree = (item: Item, clickRow: SwipeRow | undefined) => {
-    console.log('you click hidden area of item:', item.text)
+  onHiddenAreaPressed = (item: Item, clickRow: SwipeRow | undefined) => {
+    console.log('you click hidden area of item:', item.id)
     clickRow?.deleteRow(() => this.deleteDataFromList(item.id)) //在删除动画回调中再删除数据，防止动画未结束就rerender
   }
 
@@ -78,18 +77,21 @@ export default class Example extends Component<{}, State> {
     return item.text + index;
   };
 
+  //不需要可以去掉
   _ListEmptyComponent = () => (
     <View style={{ flex: 1 }}>
       <Text>Empty List</Text>
     </View>
   );
 
+  //不需要可以去掉
   _ListHeaderComponent = () => (
     <View style={{ width: WINDOW_WIDTH, justifyContent: "center", alignItems: 'center', backgroundColor: 'white', height: 40 }}>
       <Text style={{ color: 'orange', fontSize: 16, lineHeight: 19, fontWeight: "600" }}>Swipe FlatList</Text>
     </View>
   );
 
+  //不需要可以去掉
   _ListFooterComponent = () => (
     <View style={{ width: WINDOW_WIDTH, justifyContent: 'center', alignItems: 'center', height: 32 }}>
       <Text>no more data</Text>
@@ -99,7 +101,7 @@ export default class Example extends Component<{}, State> {
   _getItemLayout = (item: Item[] | null | undefined, index: number) => {
     return {
       length: 64,
-      offset: 64 * index + 40, // ListHeaderComponent.height=44
+      offset: 64 * index + 40, // ListHeaderComponent.height
       index,
     };
   };
@@ -116,7 +118,7 @@ export default class Example extends Component<{}, State> {
           ListHeaderComponent={this._ListHeaderComponent()}
           ListFooterComponent={this._ListFooterComponent()}
           keyExtrator={this.keyExtractor}
-          onHiddenAreaPress={this.onHiddenAreaPree}
+          onHiddenAreaPress={this.onHiddenAreaPressed}
           onLayout={() => console.log("on layout")}
           getItemLayout={this._getItemLayout}
         />
